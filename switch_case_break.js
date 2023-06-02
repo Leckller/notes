b266c3f4417f8656d87cfc064b26d25fa1c154e7
Leckller <7leckler@gmail.com>
2023-06-01 21:36:29 -0300

    toma le

--- switch_case_break.js
+++ switch_case_break.js
@@ -22,3 +22,36 @@ switch(fruta){
     break;
 }
 console.log(mensagem)
+
+// exercicio bacana de laços de repetição
+console.log(`ex---------------------------------------------`)
+const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = []
+let mediaAritimetica = []
+let maiorNumero = []
+let impares = []
+for (let index = 0; index < numbers.length; index++) {
+    console.log(numbers[index])
+    soma = Number(soma + numbers[index])
+    mediaAritimetica = soma / numbers.length
+    for (let index2 = 0; index2 < numbers.length; index2++) {
+        let comparacao = numbers[index]
+        if (comparacao > maiorNumero) {
+            maiorNumero = comparacao
+        }
+    }
+    // Preciso saber quantos valores ímpares o vetor tem, e caso nao tenha nenhum imprimir um mensagem.
+    const calculoImpares = numbers[index] % 2
+    if(calculoImpares != 0){
+    impares.push(numbers[index])
+    }
+}
+console.log(`A soma destes números é ${soma}`)
+if (mediaAritimetica > 20) {
+    console.log(`O valor da média aritmética é maior que 20.`)
+}
+else if (mediaAritimetica < 20) {
+    console.log(`O valor da média aritmética é menor que 20`)
+}
+console.log(`${maiorNumero} é o maior número do vetor`)
+console.log(`${impares.length} é a quantidade de números ímpares `)
